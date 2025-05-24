2bc779fae4235e4d5af1c5bc9e1bbe61106be1a3
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-25 03:39:45 +0700

    Create head.js

---
+++ app/head.js
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>Lure-TetraMON</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="farcaster-miniapp-ready" content="true" />
+      <meta name="farcaster-miniapp-name" content="Lure-TetraMON" />
+      <meta name="farcaster-miniapp-description" content="Tetris game with explosions and scoring" />
+      <meta name="farcaster-miniapp-icon" content="/icon-192.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/icon-192.png" />
+      <link rel="apple-touch-icon" href="/icon-192.png" />
+    </>
+  );
+}

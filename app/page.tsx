40e5dc38f20342824bfeded2bb1f88010fef35f4
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-25 03:56:32 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,7 @@ export const metadata = {
     "fc:frame": "vNext",
     "fc:frame:image": "https://lure-tetra-mon.vercel.app/images/splash.png",
     "fc:frame:button:1": "Play",
-    "fc:frame:post_url": "https://lure-tetra-mon.vercel.app/api/frame",
-  },
+  
 };
 
 export default function GamePage() {

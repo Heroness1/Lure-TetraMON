248ccbf5b5af500e3e6216d3615d42bb9594859c
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-25 03:59:51 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export const metadata = {
     "fc:frame": "vNext",
     "fc:frame:image": "https://lure-tetra-mon.vercel.app/images/splash.png",
     "fc:frame:button:1": "Play",
-  
-};
+  },  
+};  
 
 export default function GamePage() {
   return <NadShoott />
